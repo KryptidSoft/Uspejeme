d626c1545e405186dc51f7036421f9152bf18057
KryptidSoft <KryptidSoft@gmail.com>
2026-04-17 17:22:00 +0200

    Uspejeme 1.0.0 novy banner Aads

--- src/components/AadsBanner.tsx
+++ src/components/AadsBanner.tsx
@@ -1,33 +1,35 @@
 export default function AadsBanner() {
   return (
-    <div className="ad-wrapper" style={{ margin: "30px 0", textAlign: "center" }}>
-      
-      <span style={{ fontSize: "10px", color: "#888", display: "block", marginBottom: "5px" }}>
+    <div style={{
+      margin: "40px 0",
+      padding: "0 12px", // lepší okraje na mobilu
+      textAlign: "center"
+    }}>
+      <span style={{
+        fontSize: "10px",
+        color: "#888",
+        display: "block",
+        marginBottom: "6px"
+      }}>
         SPONZOROVANÝ OBSAH
       </span>
 
-      {/* Hlavní kontejner pro reklamu */}
-      <div
-        id="frame"
-        style={{
-          width: "100%",
-          margin: "auto",
-          position: "relative",
-          zIndex: 99998
-        }}
-      >
+      <div style={{
+        width: "100%",
+        maxWidth: "728px",
+        margin: "0 auto"
+      }}>
         <iframe
           data-aa="2434799"
           src="//acceptable.a-ads.com/2434799/?size=Adaptive"
           style={{
-            border: 0,
-            padding: 0,
+            border: "0",
             width: "100%",
-            height: "150px",
-            overflow: "hidden",
-            display: "block",
-            margin: "auto"
+            minHeight: "90px",   // mobil minimum
+            maxHeight: "250px",  // ochrana proti přerostlé reklamě
+            display: "block"
           }}
+          loading="lazy"
           title="A-Ads"
         />
       </div>
