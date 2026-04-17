34d301033279252897b05a785f18f47cc8e71674
KryptidSoft <KryptidSoft@gmail.com>
2026-04-17 17:50:56 +0200

    Uspejeme 1.0.0 Pridani Aads banneru

--- src/components/AadsBanner.tsx
+++ src/components/AadsBanner.tsx
@@ -1,38 +1,39 @@
 export default function AadsBanner() {
   return (
-    <div style={{
-      margin: "40px 0",
-      padding: "0 12px", // lepší okraje na mobilu
-      textAlign: "center"
-    }}>
-      <span style={{
-        fontSize: "10px",
-        color: "#888",
-        display: "block",
-        marginBottom: "6px"
-      }}>
+    <div
+      style={{
+        width: "100%",
+        margin: "40px auto",
+        position: "relative",
+        zIndex: 99998,
+        textAlign: "center",
+        padding: "0 12px",
+      }}
+    >
+      <span
+        style={{
+          fontSize: "10px",
+          color: "#888",
+          display: "block",
+          marginBottom: "6px",
+        }}
+      >
         SPONZOROVANÝ OBSAH
       </span>
 
-      <div style={{
-        width: "100%",
-        maxWidth: "728px",
-        margin: "0 auto"
-      }}>
-        <iframe
-          data-aa="2434799"
-          src="//acceptable.a-ads.com/2434799/?size=Adaptive"
-          style={{
-            border: "0",
-            width: "100%",
-            minHeight: "90px",   // mobil minimum
-            maxHeight: "250px",  // ochrana proti přerostlé reklamě
-            display: "block"
-          }}
-          loading="lazy"
-          title="A-Ads"
-        />
-      </div>
+      <iframe
+        data-aa="2434824"
+        src="//acceptable.a-ads.com/2434824/?size=Adaptive&background_color=0b1f3a"
+        style={{
+          border: "0",
+          padding: "0",
+          width: "70%",
+          minHeight: "100px",   // důležité pro mobil + bot
+          display: "block",
+          margin: "0 auto",
+        }}
+        title="A-Ads"
+      />
     </div>
   );
 }
